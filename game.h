b8d2f130949b6577f9fc6a0a7a115c5df5c4a160
hlross <hlross@umich.edu>
2022-05-04 21:53:03 -0400

    add use of position adt

--- game.h
+++ game.h
@@ -11,22 +11,39 @@ private:
     Piece * Board[8*8] = {nullptr};
     /////
     Piece * & find(int, int);
-public:
 
-    Piece * & operator[](std::string);
+
+public:
+    //i should generate an attack and defend map.
+    //i should make a vector for each sides' pieces
+
+    Piece * & operator[](Position);
 
     friend std::ostream & operator<<(std::ostream &, Game &);
 
-    void add(std::string, Pieces, int);
+    void add(Position, Pieces, Color);
 
-    void del(std::string);
-    void del(int, int);
+    void del(Position);
 
     void clear_board();
 
     void populate();
 
-    void move_piece(std::string, std::string);
+    bool legality_test(std::string, std::string);
+    //need to check based on two conditions:
+    //if the king is not in check:
+    //  the moving piece is not absolutely pinned on it's move direction
+    //  en passant requires special horizontal pin test of both involved pawns, which disappear from the same rank
+    //  while castling, rook is not pinned horizontally
+    //
+    //if the king is in check:
+    //  double check: only king moves to non-attacked squares, sliding check x-rays the king
+    //  single check:
+    //  capture of checking piece. the capturing piece is not absolutely pinned
+    //  king moves to non-attacked sqaures, sliding check x-rays the king
+    //  interposing moves in case of distance sliding check. the moving piece is not absolutely pinned
+
+    void move_piece(Position, Position);
 
     ~Game();
     //normal constructor
